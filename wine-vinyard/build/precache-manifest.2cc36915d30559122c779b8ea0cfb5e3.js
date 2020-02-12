5f4329989888a20abe60e604b0d1f7f56b5fed46
Darren Rose <thorn086@gmail.com>
2020-02-11 23:32:53 -0600

    Minor changes

---
+++ wine-vinyard/build/precache-manifest.2cc36915d30559122c779b8ea0cfb5e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0054ca7e6074f040dc5b63bbdd49394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe22edd61d6e835851ae",
+    "url": "/static/css/main.acc2961d.chunk.css"
+  },
+  {
+    "revision": "2e3c633ec7e3e1644a07",
+    "url": "/static/js/2.94bd765c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.94bd765c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe22edd61d6e835851ae",
+    "url": "/static/js/main.02c92def.chunk.js"
+  },
+  {
+    "revision": "92877c8d4cc7535c94a1",
+    "url": "/static/js/runtime-main.cba62e51.js"
+  },
+  {
+    "revision": "10b92892babb476dc84fc60608ad0eef",
+    "url": "/static/media/header.10b92892.jpg"
+  }
+]);
