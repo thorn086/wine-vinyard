936dae08c578af2b50b796fffe9dfb89bd80adcf
Darren Rose <thorn086@gmail.com>
2020-02-13 06:28:56 -0600

    changed endpoint

--- src/API.js
+++ src/API.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT:'https://polar-spire-46376.herokuapp.com/',
+    API_ENDPOINT:'https://polar-spire-46376.herokuapp.com',
     TOKEN_KEY: 'wine-vinyard-client-auth-token',
     USER_ID: 'wine-vinyard-user-id'
 }
