52b2f4f3d34e0dc1fe2a7580ffac4302b1436663
Darren Rose <thorn086@gmail.com>
2020-02-13 13:31:25 -0600

    adding css for navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,33 +10,38 @@ class NavBar extends React.Component {
 
     renderLogoutLink() {
         return (
-            <div className='NavBar__logged-in'  onClick={this.handleLogoutClick}>
-                <Link to='/' onClick={()=>window.location.reload()}> Logout</Link>
+            <div className='navigation__item' onClick={this.handleLogoutClick}>
+                <Link to='/' onClick={() => window.location.reload()}> Logout</Link>
             </div>
         )
     }
 
     toggleLoginLink() {
-        return (<div className='NavBar__not-logged-in'>
-            <Link to='/login' style={{ textDecoration: 'none' }}>Login</Link>
-            {'|'}
-            <Link to='/signup' style={{ textDecoration: 'none' }}>Sign Up</Link>
-        </div>
+        return (
+            <div>
+                <li className='navigation__item'>
+                    <Link to='/login' style={{ textDecoration: 'none', fontSize: '2rem', fontWeight: '300', color: 'white', textTransform: 'uppercase', backgroundImage: 'linear-gradient(120deg, #4c4cf2 80%, #4c4cf2 5%, white 92%', padding: '.5rem 1rem', borderRadius: '45%',border:'2px solid black', boxShadow: '0 1rem 3rem black' }}>Login</Link>
+                </li>
+                <li className='navigation__item'>
+                    <Link to='/signup' style={{ textDecoration: 'none', fontSize: '2rem', fontWeight: '300', color: 'white', textTransform: 'uppercase', backgroundImage: 'linear-gradient(120deg, #4c4cf2 80%, #4c4cf2 5%, white 92%', padding: '.5rem 1rem', borderRadius: '45%', border:'2px solid black', boxShadow: '0 1rem 3rem black' }}>Sign Up</Link>
+                </li>
+            </div>
         )
     }
     render() {
         return (
             <div className='navigation'>
                 <input type='checkbox' className='navigation__checkbox' id='navi-toggle' />
-                <label className='navigation__button'></label>
+                <label for='navi-toggle' className='navigation__button'>menu</label>
                 <div className='navigation__backgroud'>&nbsp;</div>
 
                 <nav className='navigation__nav'>
-                    <div className='navigation__list'>
+                    <ul className='navigation__list'>
                         {TokenService.hasAuthToken()
                             ? this.renderLogoutLink()
                             : this.toggleLoginLink()}
-                    </div>
+                        <Link to='/' style={{ textDecoration: 'none', fontSize: '2rem', fontWeight: '300', color: 'white', textTransform: 'uppercase', backgroundImage: 'linear-gradient(120deg, #4c4cf2 80%, #4c4cf2 5%, white 92%', padding: '.5rem 1rem', borderRadius: '45%', border:'2px solid black', boxShadow: '0 1rem 3rem black' }}>Home</Link>
+                    </ul>
                 </nav>
             </div>
         )
